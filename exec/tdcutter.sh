a8d18f62bad15a6ad673ff890b94cc01290278b8
mkobayas <mkobayas@redhat.com>
2017-09-22 15:09:45 +0900

    initial

--- exec/tdcutter.sh
+++ exec/tdcutter.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+DIRNAME=`dirname "$0"`
+
 JAVA_OPTS="-Xmx512m"
-CLASS_PATH="lib/args4j-2.33.jar:lib/ThreadDumpCutter-1.0.0.jar"
+CLASS_PATH="$DIRNAME/lib/args4j-2.33.jar:$DIRNAME/lib/ThreadDumpCutter-1.0.0.jar"
 
 java $JAVA_OPTS -classpath $CLASS_PATH org.mk300.tdcutter.Main "$@"
--- src/main/java/org/mk300/tdcutter/Main.java
+++ src/main/java/org/mk300/tdcutter/Main.java
@@ -4,22 +4,22 @@ import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		
-		ThreadDumpCutter cutter = new ThreadDumpCutter();
+    public static void main(String[] args) throws Exception {
+
+        ThreadDumpCutter cutter = new ThreadDumpCutter();
         CmdLineParser parser = new CmdLineParser(cutter);
         try {
-            parser.parseArgument(args);    
+            parser.parseArgument(args);
         } catch (CmdLineException e) {
             System.err.println(e);
             parser.printUsage(System.err);
             System.exit(1);
         }
-        if(cutter.help) {
+        if (cutter.help) {
             parser.printUsage(System.err);
             System.exit(1);
         }
 
         cutter.doMain();
-	}
+    }
 }
